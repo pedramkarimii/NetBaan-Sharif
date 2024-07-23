8e2f08b8fd06041ace2d44ef00069d90c056aeeb
pedramkarimii <pedram@9060.com>
2024-07-23 06:08:50 +0330

    :bug: Rename view classes in `urls.py` to match updated class names (`UserUpdateAPI`, `DetailAPI`, `UserListAPI`, `ChangePasswordAPI`, `UserDeleteAPI`).

--- apps/account/urls/api/user.py
+++ apps/account/urls/api/user.py
@@ -2,16 +2,16 @@ from django.urls import path
 from apps.account.views.views_api import user
 
 urlpatterns = [
-    path('user-update/<int:pk>/', user.UserUpdateView.as_view(), name='update-user'),
-    path('user-detail/<int:pk>/', user.DetailView.as_view(), name='detail-user'),
-    path('user-list/', user.UserListView.as_view(), name='list-user'),
-    path("user-change-password/<int:pk>/", user.ChangePasswordView.as_view(), name="change-password"),
-    path('user-delete/<int:pk>/', user.UserDeleteView.as_view(), name='delete-user'),
+    path('user-update/<int:pk>/', user.UserUpdateAPI.as_view(), name='update-user'),
+    path('user-detail/<int:pk>/', user.DetailAPI.as_view(), name='detail-user'),
+    path('user-list/', user.UserListAPI.as_view(), name='list-user'),
+    path("user-change-password/<int:pk>/", user.ChangePasswordAPI.as_view(), name="change-password"),
+    path('user-delete/<int:pk>/', user.UserDeleteAPI.as_view(), name='delete-user'),
 ]
 """
-Maps the URL 'user-update/<int:pk>/' to the UserUpdateView view.
-Maps the URL 'user-detail/<int:pk>/' to the DetailView view.
-Maps the URL 'user-list/' to the UserListView view.
-Maps the URL 'user-change-password/<int:pk>/' to the ChangePasswordView view.
-Maps the URL 'user-delete/<int:pk>/' to the UserDeleteView view.
+Maps the URL 'user-update/<int:pk>/' to the UserUpdateAPI.
+Maps the URL 'user-detail/<int:pk>/' to the DetailAPI.
+Maps the URL 'user-list/' to the UserListAPI.
+Maps the URL 'user-change-password/<int:pk>/' to the ChangePasswordAPI.
+Maps the URL 'user-delete/<int:pk>/' to the UserDeleteAPI.
 """
