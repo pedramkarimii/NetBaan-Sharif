3c52b5fb71f2de6e3327456495718cefe94a5860
pedramkarimii <pedram@9060.com>
2024-07-23 05:58:39 +0330

    :bug: Add pk for change password

--- apps/account/urls/api/user.py
+++ apps/account/urls/api/user.py
@@ -5,13 +5,13 @@ urlpatterns = [
     path('user-update/<int:pk>/', user.UserUpdateView.as_view(), name='update-user'),
     path('user-detail/<int:pk>/', user.DetailView.as_view(), name='detail-user'),
     path('user-list/', user.UserListView.as_view(), name='list-user'),
-    path("user-change-password/", user.ChangePasswordView.as_view(), name="change-password"),
+    path("user-change-password/<int:pk>/", user.ChangePasswordView.as_view(), name="change-password"),
     path('user-delete/<int:pk>/', user.UserDeleteView.as_view(), name='delete-user'),
 ]
 """
 Maps the URL 'user-update/<int:pk>/' to the UserUpdateView view.
 Maps the URL 'user-detail/<int:pk>/' to the DetailView view.
 Maps the URL 'user-list/' to the UserListView view.
-Maps the URL 'user-change-password/' to the ChangePasswordView view.
+Maps the URL 'user-change-password/<int:pk>/' to the ChangePasswordView view.
 Maps the URL 'user-delete/<int:pk>/' to the UserDeleteView view.
 """
